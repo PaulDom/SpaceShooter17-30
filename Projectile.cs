bf66e954a5d721fad3371927fc6f821785245888
PaulDom <54893606+PaulDom@users.noreply.github.com>
2023-05-20 18:17:43 +0300

    Add files via upload

--- Projectile.cs
+++ Projectile.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public GameObject prefabExplosion;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Obstacle")
         {
             Destroy(collision.gameObject);
+
+            GameObject effect = Instantiate(prefabExplosion, collision.transform.position, Quaternion.identity);
+            Destroy(effect, 3f);
             Destroy(gameObject);
         }
     }
